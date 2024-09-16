86124be417ac5f1b53bcec19ae28c9c592772943
thanhnhan2021998dn <thanhnhan2021998dn@gmail.com>
2024-09-16 21:40:23 +0700

    aaaa

---
+++ src/test/java/com/project/shopshoe/ShopshoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shopshoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopshoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
